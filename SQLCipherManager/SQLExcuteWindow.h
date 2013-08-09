193d3be6b84fa308c0e550a0b6ab8dc1cf09a529
Wan Yechao <wyc.jar@gmail.com>
2013-08-09 17:04:57 +0800

    add new window

---
+++ SQLCipherManager/SQLExcuteWindow.h
@@ -0,0 +1,13 @@
+//
+//  SQLExcuteWindow.h
+//  SQLCipherManager
+//
+//  Created by wanyc on 13-8-9.
+//  Copyright (c) 2013年 Wan Yechao. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SQLExcuteWindow : NSWindowController
+
+@end
---
+++ SQLCipherManager/SQLExcuteWindow.m
@@ -0,0 +1,34 @@
+//
+//  SQLExcuteWindow.m
+//  SQLCipherManager
+//
+//  Created by wanyc on 13-8-9.
+//  Copyright (c) 2013年 Wan Yechao. All rights reserved.
+//
+
+#import "SQLExcuteWindow.h"
+
+@interface SQLExcuteWindow ()
+
+@end
+
+@implementation SQLExcuteWindow
+
+- (id)initWithWindow:(NSWindow *)window
+{
+    self = [super initWithWindow:window];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)windowDidLoad
+{
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
