56046150dd08f3a98ed3c5ffdafef98065a4e75e
welsonla <wyc.jar@gmail.com>
2013-11-21 11:25:07 +0800

    create new db with format data time

---
+++ SQLCipherManager/Utility.h
@@ -0,0 +1,15 @@
+//
+//  Utility.h
+//  SQLCipherManager
+//
+//  Created by wanyc on 13-11-21.
+//  Copyright (c) 2013年 Wan Yechao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Utility : NSObject
+
++ (NSString *)formatTime:(NSDate *)date;
+
+@end
